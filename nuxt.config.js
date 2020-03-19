45d1fcaa378942b159665100ad8199e4e1d9cb3d
eadortsu <mail@eadortsu.com>
2020-03-19 10:18:14 +0000

    env

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,16 @@ export default {
    */
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || 'Corona App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content:
+          process.env.npm_package_description ||
+          'an app that will help the general public quickly access their risk level of infection as well as get reliable and up to date information on the corona virus.'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
