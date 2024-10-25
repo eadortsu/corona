a85ad209bd0e1d1c306b8a58f898865bd203f8eb
Eugene Adortsu <eugeneadortsu@gmail.com>
2024-10-25 11:00:58 +0100

    clean up fixs

--- pages/news/ghana.vue
+++ pages/news/ghana.vue
@@ -28,27 +28,7 @@
               ><v-icon>mdi-open-in-new</v-icon></v-btn
             >
             <v-btn icon><v-icon>mdi-share-variant</v-icon></v-btn>
-            <!-- <v-btn icon @click="show = !show">
-              <v-icon>{{
-                show ? 'mdi-chevron-up' : 'mdi-chevron-down'
-              }}</v-icon>
-            </v-btn>-->
           </v-card-actions>
-
-          <!--<v-expand-transition>
-            <div v-show="show">
-              <v-divider></v-divider>
-
-              <v-card-text>
-                I'm a thing. But, like most politicians, he promised more than
-                he could deliver. You won't have time for sleeping, soldier, not
-                with all the bed making you'll be doing. Then we'll go with that
-                data file! Hey, you add a one and two zeros to that or we walk!
-                You're going to do his laundry? I've got to find a way to
-                escape.
-              </v-card-text>
-            </div>
-          </v-expand-transition>-->
         </v-card>
       </v-col>
     </v-flex>
@@ -80,7 +60,6 @@ export default {
         )
         .then((response) => {
           const convert = require('xml-js')
-          // JSON responses are automatically parsed.
           const xml = response.data
           let result = convert.xml2json(xml, { compact: true, spaces: 4 })
           result = JSON.parse(result)
